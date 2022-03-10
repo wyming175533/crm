40e0a0f478a254e6f3655e9283d260605d227a4d
wangyiming <1755337368@qq.com>
2022-03-10 21:43:53 +0800

    完善添加

--- crm01/src/main/java/com/bjpowernode/crm/workbench/service/Impl/TranServiceImpl.java
+++ crm01/src/main/java/com/bjpowernode/crm/workbench/service/Impl/TranServiceImpl.java
@@ -1,16 +1,20 @@
 package com.bjpowernode.crm.workbench.service.Impl;
 
+import com.bjpowernode.crm.utils.DateTimeUtil;
 import com.bjpowernode.crm.utils.SqlSessionUtil;
 import com.bjpowernode.crm.utils.UUIDUtil;
 import com.bjpowernode.crm.workbench.dao.CustomerDao;
 import com.bjpowernode.crm.workbench.dao.TranDao;
+import com.bjpowernode.crm.workbench.dao.TranHistoryDao;
 import com.bjpowernode.crm.workbench.domain.Customer;
 import com.bjpowernode.crm.workbench.domain.Tran;
+import com.bjpowernode.crm.workbench.domain.TranHistory;
 import com.bjpowernode.crm.workbench.service.TranService;
 
 public class TranServiceImpl  implements TranService {
     private  TranDao tranDao=SqlSessionUtil.getSqlSession().getMapper(TranDao.class);
     private CustomerDao customerDao=SqlSessionUtil.getSqlSession().getMapper(CustomerDao.class);
+    private TranHistoryDao tranHistoryDao=SqlSessionUtil.getSqlSession().getMapper(TranHistoryDao.class);
 
     @Override
     public Boolean save(Tran t,String customerName) {
@@ -34,6 +38,23 @@ public class TranServiceImpl  implements TranService {
         if(count<0){
             flag=false;
         }
+
+
+        TranHistory tranHistory=new TranHistory();
+        tranHistory.setCreateBy(t.getCreateBy());
+        tranHistory.setCreateTime(DateTimeUtil.getSysTime());
+        tranHistory.setExpectedDate(t.getExpectedDate());
+        tranHistory.setId(UUIDUtil.getUUID());
+        tranHistory.setMoney(t.getMoney());
+        tranHistory.setStage(t.getStage());
+        tranHistory.setTranId(t.getId());
+
+        int createTHNum=tranHistoryDao.createTranHistory(tranHistory);
+        if(createTHNum<0){
+            flag=false;
+            System.out.println("创建交易历史失败");
+        }
+
         return flag;
     }
 }
